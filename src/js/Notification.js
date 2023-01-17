8e10ccf78dbfcedc363aa7871b5fc2030d3eb8c5
Yosif Georgiev <iodesign@live.com>
2023-01-17 20:16:19 +0200

    NEW: app

--- src/js/Notification.js
+++ src/js/Notification.js
@@ -17,7 +17,7 @@ export default class Notification {
 
   render(type, price) {
     const template = `
-<div class="notification type-${type} ${classNames({"is-danger" : type === Notification.types.HAWAIIAN})}">
+<div class="notification type-${type.toLowerCase()} ${classNames({"is-danger" : type === Notification.types.HAWAIIAN})}">
   <button class="delete"></button>
   🍕 <span class="type">${type}</span> (<span class="price">${formatCurrency(price)}</span>) has been added to your order.
 </div>
